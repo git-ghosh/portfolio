29f89c5c78f0ab57a25c5a232328912efc122e65
Antor Ghosh <antorgosh59@gmail.com>
2026-07-07 01:07:52 +0600

    Complete portfolio v1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,83 @@
 console.log("Portfolio Started 🚀");
+// Scroll Progress Bar
+
+window.addEventListener("scroll", () => {
+
+    const winScroll =
+        document.documentElement.scrollTop;
+
+    const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const scrolled = (winScroll / height) * 100;
+
+    document.getElementById("progress-bar").style.width =
+        scrolled + "%";
+
+});
+
+// Back To Top
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+topBtn.onclick = () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+};
+
+// Dark Mode
+
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.onclick = () => {
+
+    document.body.classList.toggle("light");
+
+};
+// Typing Animation
+
+new Typed("#typing",{
+
+    strings:[
+
+        "Computer Science Student",
+
+        "Web Developer",
+
+        "Linux Enthusiast",
+
+        "Cybersecurity Learner",
+
+        "Video Editor"
+
+    ],
+
+    typeSpeed:60,
+
+    backSpeed:40,
+
+    loop:true
+
+});
